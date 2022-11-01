aa9c21cea33ea113ee5af05decc97b6cf52b2080
65011223 <111363330+65011223@users.noreply.github.com>
2022-11-01 21:50:15 +0700

    Update week3-sol2.c

--- Weekly_Program/WEEK3/week3-sol2.c
+++ Weekly_Program/WEEK3/week3-sol2.c
@@ -2,3 +2,31 @@
 BMI = Weight / Height2
 เมื่อ Weight มีหน่วยเป็นกิโลกรัม และ Height มีหน่วยเป็นเมตร (เกณฑ์การแบ่งน้ำหนักโดยใช้ BMI , ต่ำกว่า 18.5 น้ำหนักต่ำกว่าเกณฑ์ , 18.5 - 24.9 ปกติ , 25.0 - 29.9 สูงกว่ามาตรฐาน , ตั้งแต่ 30.0 ขึ้นไป อ้วนเกินไป ) (Level 1)
 //solution2
+#include<stdio.h>
+int main()
+{
+    float height,weight,BMI;
+    printf("Enter your height in metre: ");
+    scanf("%f",&height);
+    printf("Enter your weight in kilogram: ");
+    scanf("%f",&weight);
+    BMI = weight/(height*height);
+    printf("BMI = %.1f\n",BMI);
+    if(BMI>=30)
+    {
+        printf("obese");
+    }
+    else if(BMI>=25)
+    {
+        printf("overweight");
+    }
+    else if(BMI>=18.5)
+    {
+        printf("normal");
+    }
+    else
+    {
+        printf("underweight");
+    }
+    return 0;
+}
