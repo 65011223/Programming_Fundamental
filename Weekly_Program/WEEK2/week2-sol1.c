be52f8763720ea0431a08c78779ae90948561dd3
65011223 <111363330+65011223@users.noreply.github.com>
2022-11-01 14:49:39 +0700

    Update week2-sol1.c

--- Weekly_Program/WEEK2/week2-sol1.c
+++ Weekly_Program/WEEK2/week2-sol1.c
@@ -1,14 +1,14 @@
-//จงเขียนโปรแกรมเพื่อรับค่าตัวเลข 2 ตัว แล้วแสดงผลบวก ลบ คูณ หาร ของตัวเลขทั้งสองตัวนั้น โดยใช้ตัวแปรน้อยที่สุด (Level 1)
-//solution 1
 #include<stdio.h>
 int main()
 {
-    int a,b ; 
+    int a,b ;
+    printf("num1: ") ;
     scanf("%d",&a);
+    printf("num2: ");
     scanf("%d",&b);
-    printf("%d\n", a+b);
-    printf("%d\n", a-b);
-    printf("%d\n", a*b);
-    printf("%d\n", a/b);
+    printf("num1 + num2 = %d\n", a+b);
+    printf("num1 - num2 = %d\n", a-b);
+    printf("num1 * num2 = %d\n", a*b);
+    printf("num1 / num2 = %d\n", a/b);
     return 0;
 }
