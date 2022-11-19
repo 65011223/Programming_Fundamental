ce38b5fe449a44cdbf5340f828a6156faded6910
65011223 <111363330+65011223@users.noreply.github.com>
2022-11-19 23:14:39 +0700

    Update week5-sol1.c

--- Weekly_Program/WEEK5/week5-sol1.c
+++ Weekly_Program/WEEK5/week5-sol1.c
@@ -1,19 +1,22 @@
+//จงเขียนโปรแกรมรับประโยค 1 ประโยคแล้วแสดงผลประโยคนั้นตรงกลางจอภาพ (Level 2)
+//solution2 เนื่องจากหนึ่งบรรทัดมีความกว้าง 80 ตัวอักษร
 #include<stdio.h>
+#include<string.h>
 #include<windows.h>
+// void gotoxy(int,int);
 int main()
 {
-    int width = GetSystemMetrics(SM_CXSCREEN);
-    int height = GetSystemMetrics(SM_CYSCREEN);
-    char str[1536];
+    char str[80];
     printf("Enter your message : ");
     scanf("%s",str);
     int length = strlen(str);
-    int blankx = (width-length)/2;
-    // int blanky = (height);
-    for(int i = 0 ; i<blankx; i++)
-    {
-        printf(" ");
-    }
+    int y=0; int x = 80/2 - length/2;
+    // gotoxy(x,y);
     printf("%s",str);
     return 0;
 }
+// void gotoxy(int x,int y)
+// {
+//     COORD c = {x,y};
+//     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), c);
+// }
